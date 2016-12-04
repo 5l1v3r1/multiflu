cba20a8e77b44fc11f95341229ea026f93be6d5c
Richard Neher <richard.neher@tuebingen.mpg.de>
2016-12-04 21:32:37 +0100

    added mutual coloring

--- js/multiTree.js
+++ js/multiTree.js
@@ -8,6 +8,25 @@ const prefix = "/data/yam_3y_";
 const tangle = d3.select("#tangle");
 tangle.attr("width", tangle_width).attr("height",height);
 
+const colorby = "ha_ladder";
+
+const r=3, highlightR = 5;
+const highlightFill = "#DA4", highlightStroke = "#C93";
+const colors =   ["#426FCE", "#4B8DC2", "#59A3AA", "#6BB18D", "#82BA71", "#9CBE5B", "#B7BD4B", "#CFB541", "#DFA43B", "#E68735", "#E35E2D", "#DD3124"];
+const genericDomain = [ 0, 0.111, 0.222, 0.333, 0.444, 0.555, 0.666, 0.777, 0.888, 1.0 ];
+
+var colorTree =function(t1, t2) {
+    var cScale = d3.scale.linear().domain(genericDomain).range(colors);
+    var nTips= trees[0].numberOfTips;
+    var cols = trees[0].nodes.map(function (d){return cScale(1.0*d.n.clade/nTips);});
+    var nTips= trees[1].numberOfTips;
+    var colsPartner = trees[1].nodes.map(function (d){return d.partners?cScale(1.0*d.partners[0].n.clade/nTips):"#CCCCCC";});
+    if (trees[0]){
+        trees[0].updateMultipleArray('.tip', {}, {fill:cols, stroke:cols.map(function(d){return d3.rgb(d).darker();})}, 0);
+        trees[1].updateMultipleArray('.tip', {}, {fill:colsPartner, stroke:colsPartner.map(function(d){return d3.rgb(d).darker();})}, 0);
+    }
+}
+
 var branchHover = function(node){
     console.log("Hover", node.n.strain);
     for (var ti=0; ti<trees.length; ti++){
@@ -17,31 +36,31 @@ var branchHover = function(node){
     }
     const tanglesToUpdate = [];
     var makeCallback = function(){
-	const ttu =tanglesToUpdate;
-	return function(d){
-	if (d.terminal){
-	d.selected=true;
-	for (var i=0; i<d.partners.length; i++){
-	    d.partners[i].selected=true;
-	}
-	for (var i=0; i<d.tangles.length; i++){
-	    ttu.push(d.tangles[i]);
-	}
-	}
-    };
+            const ttu =tanglesToUpdate;
+            return function(d){
+            if (d.terminal){
+                d.selected=true;
+                for (var i=0; i<d.partners.length; i++){
+                    d.partners[i].selected=true;
+                }
+                for (var i=0; i<d.tangles.length; i++){
+                    ttu.push(d.tangles[i]);
+                }
+            }
+        };
     };
     applyToChildren(node, makeCallback());
     for (var ti=0; ti<trees.length; ti++){
-	const attrs = {'r': trees[ti].nodes.map(function(d){return d.selected?8:3;})};
-	const styles = {'fill': trees[ti].nodes.map(function(d){return d.selected?"#DA4":"#CCC";}),
-			'stroke': trees[ti].nodes.map(function(d){return d.selected?"#C93":"#CCC";})};
-	    trees[ti].updateMultipleArray('.tip', attrs, styles, dt=0);
+    const attrs = {'r': trees[ti].nodes.map(function(d){return d.selected?highlightR:r;})};
+    const styles = {'fill': trees[ti].nodes.map(function(d){return d.selected?highlightFill:d.fill;}),
+                    'stroke': trees[ti].nodes.map(function(d){return d.selected?highlightStroke:d.fill;})};
+        trees[ti].updateMultipleArray('.tip', attrs, styles, dt=0);
     }
     for (var ti=0; ti<tanglesToUpdate.length; ti++){
-	tangle.select("#tangle_"+tanglesToUpdate[ti])
+    tangle.select("#tangle_"+tanglesToUpdate[ti])
         .style("stroke","#DA4")
         .style("stroke-width",3);
-    } 
+    }
 };
 
 var branchMouseOut = function(node){
@@ -50,13 +69,8 @@ var branchMouseOut = function(node){
             trees[ti].nodes[ni].selected=false;
         }
     }
-    for (var ti=0; ti<trees.length; ti++){
-	const attrs = {'r': trees[ti].nodes.map(function(d){return d.selected?8:3;})};
-	const styles = {'fill': trees[ti].nodes.map(function(d){return d.selected?"#DA4":"#CCC";}),
-			'stroke': trees[ti].nodes.map(function(d){return d.selected?"#C93":"#CCC";})};
-	    trees[ti].updateMultipleArray('.tip', attrs, styles, dt=0);
-    }
-	tangle.selectAll(".tangles")
+    colorTree();
+    tangle.selectAll(".tangles")
         .style("stroke","#CCC")
         .style("stroke-width",2);
 };
@@ -68,15 +82,14 @@ var branchClick = function(node){
 var tipHover = function(tip){
   for (var ti=0; ti<trees.length; ti++){
       trees[ti].svg.select("#tip_"+tip.n.clade)
-        .attr("r", 8)
-        .style("stroke", function(d) {return "#DA4";})
-        .style("stroke-dasharray", function(d) {return "none";})
-        .style("fill", function(d) { return "#C93";});
+        .attr("r", highlightR)
+        .style("stroke", function(d) {return highlightStroke;})
+        .style("fill", function(d) { return highlightFill;});
   }
   for (var ci=0; ci<tip.tangles.length; ci++){
     tangle.select("#tangle_"+tip.tangles[ci])
-        .style("stroke","#DA4")
-        .style("stroke-width",3);
+        .style("stroke",highlightFill)
+        .style("stroke-width",r);
   }
 };
 
@@ -170,6 +183,7 @@ var makeTangle = function(){
         })
         .style("stroke",function(){return "#CCC";})
         .style("stroke-width",function(){return 2;});
+    colorTree();
 }
 
 
